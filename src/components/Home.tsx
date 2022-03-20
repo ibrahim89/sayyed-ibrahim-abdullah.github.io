acece12103ed3104a22d0714dd4c5ec3838f6a39
Tejesh Raut <tejesh22.raut@gmail.com>
2022-03-21 02:56:23 +0530

    Added Home Screen

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,22 @@
 import * as React from 'react';
+import Typography from '@mui/material/Typography';
 
 export const Home = () => {
-  return (<div style={{ marginTop: '15vh' }}> Home will be displayed here
+  return (<div className={'home'}>
+    <div className={'home-text'}>
+      <Typography component={'h4'} variant={'h4'} align={'left'}>
+        <span className={'intro'}> Hi, I'm</span>
+      </Typography>
+      <Typography component={'h3'} variant={'h3'} align={'left'}>
+        <span className={'intro'}>
+          <span className={'intro-initial'}>T</span>ejesh &nbsp;
+          <span className={'intro-initial'}>R</span>aut
+        </span>
+      </Typography>
+      <Typography component={'span'} variant={'body1'} align={'left'}>
+        <p className={'designation'}>Software Engineer at Google</p>
+        <p className={'designation'}>B.Tech. in Computer Science, IIT Bombay</p>
+      </Typography>
+    </div>
   </div>);
 };
