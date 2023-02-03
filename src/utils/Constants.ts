c6b3776926bb9227993ca2b2c6625b7a7e09c3c9
ibrahim89 <hafizeabdullah@gmail.com>
2023-02-03 20:03:53 +0530

    Updates

--- src/utils/Constants.ts
+++
@@ -1,4 +0,0 @@
-export abstract class Constants {
-  static readonly themeType: { [k: string]: ThemeType } = { light: 'light', dark: 'dark' };
-  static readonly routes: string[] = ['/', 'about', 'education', 'experience', 'skills', 'projects', 'contact'];
-}
